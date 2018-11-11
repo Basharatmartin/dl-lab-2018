097237a4539280cb176f15ab8db6f8cf3d63b566
Basharat Martin <bbasharat@basecon.de>
2018-11-11 06:12:38 +0100

    added cnn_tf.py file

---
+++ exercise2/cnn_tf.py
@@ -0,0 +1,14 @@
+### Author : Basharat Basharat
+### Email  : basharat.basharat@uranus.uni-freiburg.de
+
+from __future__ import print_function
+
+import tensorflow as tf
+import numpy as np
+
+class Cnn_Tensorflow ():
+
+	def __init__ (self, ):
+		self.
+
+
